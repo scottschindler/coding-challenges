9248abc9a8136d98e10e2954e5e6bc14534132b1
scott schindler <scottschindler29@gmail.com>
2022-08-15 15:52:52 -0400

    added VM capabilities

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -3,28 +3,77 @@ import { useRouter } from "next/router";
 import sdk from "@stackblitz/sdk";
 
 export default function challenges() {
-  useEffect(() => {
-    sdk.openProjectId(`${process.env.PROJECT_ID}`, {
-      newWindow: false,
-      openFile: "src/App.tsx",
-      view: "preview",
+  let vm;
+  async function goToProject() {
+    let test = "testhfjbdjhbfgdjh";
+    vm = await sdk.embedGithubProject(
+      "embed",
+      "scottschindler/coding-challenges",
+      {
+        height: 400,
+        //   openFile: `userId/${test}.js`,
+        openFile: "pages/index.tsx",
+        //   openFile: angular.openFile,
+      }
+    );
+
+    for (const button of document.querySelectorAll<HTMLButtonElement>(
+      "button:disabled"
+    )) {
+      button.disabled = false;
+    }
+
+    if (!vm) {
+      console.error("SDK vm is not available");
+      return;
+    }
+
+    await vm.applyFsDiff({
+      create: {
+        "testing.html": `${test}`,
+      },
+      destroy: [],
     });
-  }, []);
+  }
 
   const router = useRouter();
 
-  const generateUniqueForkedStackBlitzLink = (id: string) => {
-    console.log("yo");
+  const getFiles = async () => {
+    const files = await vm.getFsSnapshot();
+    console.log(files);
+  };
+  const embedNewProjectWithFiles = async () => {
+    const files = await vm.getFsSnapshot();
+    sdk.embedProject(
+      "embed2",
+      {
+        title: "Node Starter",
+        description: "A basic Node.js project",
+        template: "node",
+        files: JSON.parse(files),
+      },
+      {
+        clickToLoad: true,
+        openFile: "index.js",
+        terminalHeight: 50,
+      }
+    );
   };
 
   return (
-    <div style={{ height: "100%" }}>
+    <div>
       <ul>
-        <li onClick={() => router.push("/challenges/1")}>React 1</li>
+        <li onClick={goToProject}>React 1</li>
+        <div id="embed"></div>
+        <div id="embed2"></div>
+
         <li>React 1</li>
         <li>React 1</li>
         <li>React 1</li>
       </ul>
+
+      <button onClick={getFiles}>Check</button>
+      <button onClick={embedNewProjectWithFiles}>Check again</button>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,18 @@ import { useRouter } from "next/router";
 const Home: NextPage = () => {
   const router = useRouter();
   return (
-    <div style={{ height: "100%" }}>
-      <h1>yo</h1>
+    <div className="flex  flex-col w-full h-screen border items-center justify-center">
+      <p className="text-5xl font-bold text-center w-2/3">
+        Level up your React and Node skills quickly
+      </p>
+      <p></p>
       <button
+        className=""
         onClick={() => {
           router.push("/challenges");
         }}
       >
-        Get Started
+        Sign up for Waitlist
       </button>
     </div>
   );
