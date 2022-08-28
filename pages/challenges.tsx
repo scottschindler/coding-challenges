15248714a1b53e26d759b43c03c107ea52af474d
Scott Schindler <scottschindler29@gmail.com>
2022-08-28 15:40:19 -0400

    changed from scott+manuel meeting

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -3,65 +3,83 @@ import { useRouter } from "next/router";
 import sdk from "@stackblitz/sdk";
 
 export default function challenges() {
-  let vm;
+  let originalProject;
+  let newProject;
+
   async function goToProject() {
     let test = "testhfjbdjhbfgdjh";
-    vm = await sdk.embedGithubProject(
-      "embed",
-      "scottschindler/coding-challenges",
-      {
-        height: 400,
-        //   openFile: `userId/${test}.js`,
-        openFile: "pages/index.tsx",
-        //   openFile: angular.openFile,
-      }
-    );
+    // originalProject = await sdk.embedGithubProject(
+    //   "embed",
+    //   "scottschindler/coding-challenges",
+    //   {
+    //     height: 400,
+    //     //   openFile: `userId/${test}.js`,
+    //     openFile: "pages/index.tsx",
+    //     //   openFile: angular.openFile,
+    //   }
+    // );
 
-    for (const button of document.querySelectorAll<HTMLButtonElement>(
-      "button:disabled"
-    )) {
-      button.disabled = false;
-    }
-
-    if (!vm) {
-      console.error("SDK vm is not available");
-      return;
-    }
-
-    await vm.applyFsDiff({
-      create: {
-        "testing.html": `${test}`,
-      },
-      destroy: [],
+    sdk.openGithubProject("scottschindler/coding-challenges", {
+      newWindow: true,
     });
+
+    // sdk.openProjectId("nextjs-7kdwma", {
+    //   newWindow: true,
+    //   view: "editor",
+    // });
+
+    // await originalProject.applyFsDiff({
+    //   create: {
+    //     "testing.js": test,
+    //   },
+    //   destroy: [Object.keys(originalProject).flat()],
+    // });
+    // const files = await originalProject.getFsSnapshot();
+    // console.log(files);
   }
 
   const router = useRouter();
 
-  const getFiles = async () => {
-    const files = await vm.getFsSnapshot();
-    console.log(files);
-  };
+  // const getFiles = async () => {
+  //   const files = await vm.getFsSnapshot();
+  //   console.log(files);
+  // };
+
   const embedNewProjectWithFiles = async () => {
-    const files = await vm.getFsSnapshot();
-    sdk.embedProject(
-      "embed2",
-      {
-        title: "Node Starter",
-        description: "A basic Node.js project",
-        template: "node",
-        files: JSON.parse(files),
-      },
-      {
-        clickToLoad: true,
-        openFile: "index.js",
-        terminalHeight: 50,
-      }
-    );
+    const files = await originalProject.getFsSnapshot();
+    console.log(files);
+    newProject = await sdk.embedProjectId("embed2", "node-s1witt", {
+      forceEmbedLayout: true,
+    });
+
+    await newProject.applyFsDiff({
+      create: files,
+      destroy: [],
+    });
+
+    // sdk.embedProject(
+    //   "embed2",
+    //   {
+    //     title: "Node Starter",
+    //     description: "A basic Node.js project",
+    //     template: "node",
+    //     files: { files },
+    //   },
+    //   {
+    //     clickToLoad: true,
+    //     openFile: "index.js",
+    //     terminalHeight: 50,
+    //   }
+    // );
+  };
+
+  const getURL = async () => {
+    const url = await vm.preview.getUrl();
+    console.log(url);
   };
 
   return (
-    <div>
+    <div className="flex flex-col">
       <ul>
         <li onClick={goToProject}>React 1</li>
         <div id="embed"></div>
@@ -72,8 +90,19 @@ export default function challenges() {
         <li>React 1</li>
       </ul>
 
-      <button onClick={getFiles}>Check</button>
-      <button onClick={embedNewProjectWithFiles}>Check again</button>
+      {/* <button onClick={getFiles}>Check</button> */}
+      <button onClick={embedNewProjectWithFiles}>
+        embedNewProjectWithFiles
+      </button>
+      <button onClick={getURL}>get URL</button>
+      <iframe
+        src="https://codesandbox.io/embed/currying-sky-1pjgpd?fontsize=14&hidenavigation=1&theme=dark"
+        // style={{width:"100%", height:"500px", border:0, border-radius: "4px", overflow:"hidden"}}
+        style={{ width: "100%", height: "500px", border: 0 }}
+        title="currying-sky-1pjgpd"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
     </div>
   );
 }
